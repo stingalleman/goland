3502ab290068104e5a4a7139295e8491b1956e77
Sting Alleman <stingalleman@icloud.com>
2021-03-04 09:21:06 +0100

    cleaner

--- cursor/move.go
+++ cursor/move.go
@@ -9,44 +9,45 @@ type Data struct {
 	Character rune
 }
 
-// MoveU aa
+// MoveUp aa
 func MoveUp(data *Data, term *term.Data) {
 	if DetectObjectUp(data, term) {
-	} else {
-		term.Screen.SetContent(data.X, data.Y, ' ', nil, term.Style)
-		data.Y--
-		term.Screen.SetContent(data.X, data.Y, data.Character, nil, term.Style)
+		return
 	}
+	term.Screen.SetContent(data.X, data.Y, ' ', nil, term.Style)
+	data.Y--
+	term.Screen.SetContent(data.X, data.Y, data.Character, nil, term.Style)
+
 }
 
 // MoveDown moves down.
 func MoveDown(data *Data, term *term.Data) {
 	if DetectObjectDown(data, term) {
-	} else {
-		term.Screen.SetContent(data.X, data.Y, ' ', nil, term.Style)
-		data.Y++
-		term.Screen.SetContent(data.X, data.Y, data.Character, nil, term.Style)
+		return
 	}
+	term.Screen.SetContent(data.X, data.Y, ' ', nil, term.Style)
+	data.Y++
+	term.Screen.SetContent(data.X, data.Y, data.Character, nil, term.Style)
 }
 
 // MoveRight moves right.
 func MoveRight(data *Data, term *term.Data) {
 	if DetectObjectRight(data, term) {
-	} else {
-		term.Screen.SetContent(data.X, data.Y, ' ', nil, term.Style)
-		data.X++
-		term.Screen.SetContent(data.X, data.Y, data.Character, nil, term.Style)
+		return
 	}
+	term.Screen.SetContent(data.X, data.Y, ' ', nil, term.Style)
+	data.X++
+	term.Screen.SetContent(data.X, data.Y, data.Character, nil, term.Style)
 }
 
 // MoveLeft moves left.
 func MoveLeft(data *Data, term *term.Data) {
 	if DetectObjectLeft(data, term) {
-	} else {
-		term.Screen.SetContent(data.X, data.Y, ' ', nil, term.Style)
-		data.X--
-		term.Screen.SetContent(data.X, data.Y, data.Character, nil, term.Style)
+		return
 	}
+	term.Screen.SetContent(data.X, data.Y, ' ', nil, term.Style)
+	data.X--
+	term.Screen.SetContent(data.X, data.Y, data.Character, nil, term.Style)
 }
 
 // PlaceCursor places the cursor at a location.
